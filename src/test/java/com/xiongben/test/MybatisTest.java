7812558d7707a551e9cff11340f161532d98edbf
xiongben <xiongben1993@gmail.com>
2019-12-06 16:28:09 +0800

    [add]

---
+++ src/test/java/com/xiongben/test/MybatisTest.java
@@ -0,0 +1,28 @@
+package com.xiongben.test;
+
+
+import com.xiongben.dao.IUserDao;
+import com.xiongben.domain.User;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.InputStream;
+import java.util.List;
+
+public class MybatisTest {
+    public static void main(String[] args)throws Exception {
+        InputStream in = Resources.getResourceAsStream("SqlMapConfig.xml");
+        SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
+        SqlSessionFactory factory = builder.build(in);
+        SqlSession session = factory.openSession();
+        IUserDao userDao = session.getMapper(IUserDao.class);
+        List<User> users = userDao.findAll();
+        for (User user : users){
+            System.out.println(user);
+        }
+        session.close();
+        in.close();
+    }
+}
